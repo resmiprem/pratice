822a53db8e2159eb58ad7981cbae3ad845ba9d8d
resmiprem <58774810+resmiprem@users.noreply.github.com>
2021-03-04 08:15:30 +0530

    Add files via upload

---
+++ Day12/ExecutorThreadFactory.java
@@ -0,0 +1,17 @@
+package Day12;
+import java.util.concurrent.ExecutorService; 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory; 
+public class ExecutorThreadFactory {
+	static Thread t;
+	public static void main(String[] args) {   
+		ExecutorService es=Executors.newFixedThreadPool(1,new ThreadFactory() 
+				{          @Override 
+			public Thread newThread(Runnable r) {
+					t=Executors.defaultThreadFactory().newThread(r); 
+					t.setName("thread factory thread...");             
+					System.out.println("thread...................");  
+					return t;          }       });   
+		es.execute(()->{
+	System.out.println("thread called...........");});    
+		System.out.println(t);    } }
