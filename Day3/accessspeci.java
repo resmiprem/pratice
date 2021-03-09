aeb45234e9e12fc343753d2a4cbf5e7c51b596d8
resmiprem <58774810+resmiprem@users.noreply.github.com>
2021-03-09 08:08:25 +0530

    Add files via upload

---
+++ Day3/accessspeci.java
@@ -0,0 +1,26 @@
+package ey;
+
+public class accessspeci {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+System.out.println("hello world");
+	}
+
+}
+class day1 extends accessspeci{
+	private int number;
+	public int no2;
+	protected int no3;
+	int no4;
+	public void met() {
+	System.out.println(number);
+	 System.out.println(no2);
+	 System.out.println(no3);
+	 System.out.println(no4);
+	}
+}
+class day2{
+	
+}
